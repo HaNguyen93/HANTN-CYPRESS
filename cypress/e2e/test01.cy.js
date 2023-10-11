cbb126e6e67ada3bb06c32922de5aadf0e9562b3
hanguyen <nguyenhak21@gmail.com>
2023-10-11 17:00:37 +0700

    first commit

---
+++ cypress/e2e/test01.cy.js
@@ -0,0 +1,5 @@
+describe('template spec 01', () => {
+  it('test01', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
---
+++ cypress/e2e/test02.cy.js
@@ -0,0 +1,5 @@
+describe('template spec 02', () => {
+  it('test02', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
